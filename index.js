52cf3fda167575f1485e8e71df0276a6767af688
kalium <martijn@becker.wtf>
2017-07-31 15:17:58 +0200

    Added basics.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 'use strict'
+// Imports.
+const { app, BrowserWindow } = require('electron')
+
+
+/*
+ * Creating the window
+ */
+
+// Global variable for window object.
+const win
+const windowConfig = {
+	frame: false,
+	fullscreen: true,
+	thickFrame: false,
+	fullscreenable: true,
+}
+
+// Don't do anything till the app is ready.
+app.once('ready', _ => {
+	// The window must be assigned to a variable else the gc will eat it.
+	 win = new BrowserWindow(windowConfig)
+})
+
+
